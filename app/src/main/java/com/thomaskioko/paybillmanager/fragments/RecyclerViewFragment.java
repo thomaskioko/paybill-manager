44e7f2ecd1351d7511fb75dc8b0c579a8e487276
kioko <kiokotomas@gmail.com>
2016-06-06 15:50:57 +0300

    Remove unused imports

--- app/src/main/java/com/thomaskioko/paybillmanager/fragments/RecyclerViewFragment.java
+++ app/src/main/java/com/thomaskioko/paybillmanager/fragments/RecyclerViewFragment.java
@@ -3,7 +3,6 @@ package com.thomaskioko.paybillmanager.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
