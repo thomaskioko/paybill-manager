7ebc3d51b8b4df2ff13cb995dd62a401f45b2d42
kioko <kiokotomas@gmail.com>
2016-06-13 23:25:56 +0300

    Optimise imports

--- app/src/test/java/com/thomaskioko/paybillmanager/ExampleUnitTest.java
+++ app/src/test/java/com/thomaskioko/paybillmanager/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.thomaskioko.paybillmanager;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
