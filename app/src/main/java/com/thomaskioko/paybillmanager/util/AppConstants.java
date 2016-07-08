933a91f2702b04e122118aa2d73e588bc12ce4a4
kioko <kiokotomas@gmail.com>
2016-07-08 11:39:28 +0300

    Create class containing constants used across the application.

---
+++ app/src/main/java/com/thomaskioko/paybillmanager/util/AppConstants.java
@@ -0,0 +1,13 @@
+package com.thomaskioko.paybillmanager.util;
+
+import android.support.v4.content.LocalBroadcastManager;
+
+/**
+ * @author Thomas Kioko
+ */
+public class AppConstants {
+    /**
+     * Tag used by {@link LocalBroadcastManager}
+     */
+    public static final String RELOAD_PAYBILLS = "reloadPayBillsTag";
+}
