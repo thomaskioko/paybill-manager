c0725106ec413b5eda59e0bc14219504c9e95647
kioko <kiokotomas@gmail.com>
2018-06-07 18:31:48 +0300

    create api test classes

---
+++ safaricom_api/src/test/java/com/thomaskioko/safaricomapi/api/ApiResponseTest.kt
@@ -0,0 +1,43 @@
+package com.thomaskioko.safaricomapi.api
+
+import com.thomaskioko.daraja.repository.api.util.ApiErrorResponse
+import com.thomaskioko.daraja.repository.api.util.ApiResponse
+import com.thomaskioko.daraja.repository.api.util.ApiSuccessResponse
+import okhttp3.MediaType
+import okhttp3.ResponseBody
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Response
+
+@RunWith(JUnit4::class)
+class ApiResponseTest {
+    @Test
+    fun exception() {
+        val exception = Exception("foo")
+        val (errorMessage) = ApiResponse.create<String>(exception)
+        assertThat<String>(errorMessage, `is`("foo"))
+    }
+
+    @Test
+    fun success() {
+        val apiResponse: ApiSuccessResponse<String> = ApiResponse
+                .create<String>(Response.success("foo")) as ApiSuccessResponse<String>
+        assertThat<String>(apiResponse.body, `is`("foo"))
+        assertThat<Int>(apiResponse.nextPage, `is`(nullValue()))
+    }
+
+
+    @Test
+    fun error() {
+        val errorResponse = Response.error<String>(
+                400,
+                ResponseBody.create(MediaType.parse("application/txt"), "blah")
+        )
+        val (errorMessage) = ApiResponse.create<String>(errorResponse) as ApiErrorResponse<String>
+        assertThat<String>(errorMessage, `is`("blah"))
+    }
+}
