8c8094041928645b0f0db4a00d6b4d7a2dcc2220
kioko <kiokotomas@gmail.com>
2018-06-07 18:35:48 +0300

    update response pojo

--- safaricom_api/src/main/java/com/thomaskioko/daraja/repository/api/service/SafaricomRequestService.kt
+++ safaricom_api/src/main/java/com/thomaskioko/daraja/repository/api/service/SafaricomRequestService.kt
@@ -2,6 +2,7 @@ package com.thomaskioko.daraja.repository.api.service
 
 import android.arch.lifecycle.LiveData
 import com.thomaskioko.daraja.repository.api.util.ApiResponse
+import com.thomaskioko.daraja.repository.db.entity.PushRequestResponse
 import com.thomaskioko.daraja.repository.db.entity.SafaricomPushRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,5 +15,5 @@ interface SafaricomService {
     val tasks: LiveData<ApiResponse<SafaricomPushRequest>>
 
     @POST("mpesa/stkpush/v1/processrequest")
-    fun sendPush(@Body safaricomPushRequest: SafaricomPushRequest): LiveData<ApiResponse<SafaricomPushRequest>>
+    fun sendPushRequest(@Body safaricomPushRequest: SafaricomPushRequest): LiveData<ApiResponse<PushRequestResponse>>
 }
