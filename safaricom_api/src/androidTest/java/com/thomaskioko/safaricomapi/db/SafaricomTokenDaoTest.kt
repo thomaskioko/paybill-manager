7fb548bda60f95fcfde70da8d0cd57e8d65b0c9c
kioko <kiokotomas@gmail.com>
2018-06-07 16:31:33 +0300

    add token dao test class

---
+++ safaricom_api/src/androidTest/java/com/thomaskioko/safaricomapi/db/SafaricomTokenDaoTest.kt
@@ -0,0 +1,45 @@
+package com.thomaskioko.safaricomapi.db
+
+import android.support.test.runner.AndroidJUnit4
+import com.thomaskioko.safaricomapi.util.LiveDataTestUtil.getValue
+import com.thomaskioko.safaricomapi.util.TestUtil
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SafaricomTokenDaoTest : SafaricomDbTest() {
+
+
+    @Test
+    fun insertLoadToken() {
+
+        val safaricomToken = TestUtil.createToken()
+        db.safaricomTokenDao().insertSafaricomToken(safaricomToken)
+
+        val loadedToken = getValue(db.safaricomTokenDao().getAccessToken())
+
+        //Verify that data from db is same as data created
+        assertThat(loadedToken.accessToken, `is`(safaricomToken.accessToken))
+    }
+
+    @Test
+    fun updateAndLoad() {
+
+        val safaricomToken = TestUtil.createToken()
+        db.safaricomTokenDao().insertSafaricomToken(safaricomToken)
+
+        val updatedSafaricomToken = TestUtil.createToken("vK13flUwxNhIsYPzvNAJwslDnruQE")
+        db.safaricomTokenDao().updateSafaricomToken(updatedSafaricomToken)
+
+        val loadedToken = getValue(db.safaricomTokenDao().getAccessToken())
+
+        //Verify that data from db is same as data updated
+        assertThat(loadedToken.accessToken, `is`(updatedSafaricomToken.accessToken))
+
+
+    }
+}
+
