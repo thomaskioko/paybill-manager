70c86f6be7605ba0011ce5d9254fea2574c9121f
kioko <kiokotomas@gmail.com>
2018-06-08 11:43:12 +0300

    add date-time type converter

--- safaricom_api/src/main/java/com/thomaskioko/daraja/repository/db/SafaricomDb.kt
+++ safaricom_api/src/main/java/com/thomaskioko/daraja/repository/db/SafaricomDb.kt
@@ -2,6 +2,8 @@ package com.thomaskioko.daraja.repository.db
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.TypeConverters
+import com.thomaskioko.daraja.repository.db.converter.DateTimeTypeConverters
 import com.thomaskioko.daraja.repository.db.dao.SafaricomPushRequestDao
 import com.thomaskioko.daraja.repository.db.dao.SafaricomTokenDao
 import com.thomaskioko.daraja.repository.db.entity.PushRequestResponse
@@ -11,9 +13,14 @@ import com.thomaskioko.daraja.repository.db.entity.SafaricomToken
 /**
  * Main database description.
  */
-@Database(entities = [
-    SafaricomToken::class, PushRequestResponse::class],
-        version = 1)
+@Database(
+        entities = [
+            SafaricomToken::class,
+            PushRequestResponse::class
+        ],
+        version = 1
+)
+@TypeConverters(DateTimeTypeConverters::class)
 abstract class SafaricomDb : RoomDatabase() {
 
     abstract fun safaricomTokenDao(): SafaricomTokenDao
