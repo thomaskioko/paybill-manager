aea41a80f8f2ee20fa18f26213681eb1f76efbe9
kioko <kiokotomas@gmail.com>
2018-06-08 11:36:13 +0300

    initialize expireTime in class and not the constructor

--- safaricom_api/src/main/java/com/thomaskioko/daraja/repository/db/entity/SafaricomToken.kt
+++ safaricom_api/src/main/java/com/thomaskioko/daraja/repository/db/entity/SafaricomToken.kt
@@ -3,6 +3,8 @@ package com.thomaskioko.daraja.repository.db.entity
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.ZoneId
 
 
 @Entity
@@ -10,11 +12,11 @@ data class SafaricomToken(
         @SerializedName("expires_in")
         var expiresIn: String,
         @SerializedName("access_token")
-        var accessToken: String,
-        var expireTime: Long
+        var accessToken: String
 ) {
 
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
+    var expireTime: OffsetDateTime = OffsetDateTime.now(ZoneId.of("Z")).plusSeconds(expiresIn.toLong())
 
 }
