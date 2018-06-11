61ca7ee6d660745aaa8e46d601079245287f7111
kioko <kiokotomas@gmail.com>
2018-06-12 00:09:51 +0300

    rename class to match class definition

--- safaricom_api/src/main/java/com/thomaskioko/daraja/api/service/SafaricomRequestService.kt
+++ safaricom_api/src/main/java/com/thomaskioko/daraja/api/service/SafaricomRequestService.kt
@@ -1,18 +1,18 @@
 package com.thomaskioko.daraja.api.service
 
 import android.arch.lifecycle.LiveData
-import com.thomaskioko.daraja.model.SafaricomPushRequest
 import com.thomaskioko.daraja.api.util.ApiResponse
 import com.thomaskioko.daraja.db.entity.PushRequestResponse
+import com.thomaskioko.daraja.model.SafaricomPushRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
 
-interface SafaricomService {
+interface SafaricomRequestService {
 
-    @get:GET("jobs/pending")
-    val tasks: LiveData<ApiResponse<SafaricomPushRequest>>
+    @GET("jobs/pending")
+    fun getPendingTasks(): LiveData<ApiResponse<SafaricomPushRequest>>
 
     @POST("mpesa/stkpush/v1/processrequest")
     fun sendPushRequest(@Body safaricomPushRequest: SafaricomPushRequest): LiveData<ApiResponse<PushRequestResponse>>
