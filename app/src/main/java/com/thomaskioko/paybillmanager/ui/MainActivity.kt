485d571490b964791105154f5c60f93798f9c68b
kioko <kiokotomas@gmail.com>
2018-06-14 15:31:19 +0300

    create activities

---
+++ app/src/main/java/com/thomaskioko/paybillmanager/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.thomaskioko.paybillmanager.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.thomaskioko.paybillmanager.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/thomaskioko/paybillmanager/ui/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.thomaskioko.paybillmanager.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.thomaskioko.paybillmanager.R
+import io.reactivex.schedulers.Schedulers
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+
+            Schedulers.io().createWorker().schedule({
+
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+
+            })
+            finish()
+        }, 1500)
+    }
+}
