9e8c945f75d94beb3f302db72c147e0696ac3126
Thomas Kioko <kiokotomas@gmail.com>
2018-06-16 13:52:23 +0300

    update imports

--- app/src/androidTest/java/com/thomaskioko/paybillmanager/ExampleInstrumentationTest.java
+++ app/src/androidTest/java/com/thomaskioko/paybillmanager/ExampleInstrumentationTest.java
@@ -1,12 +1,13 @@
 package com.thomaskioko.paybillmanager;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.MediumTest;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.filters.MediumTest;
+import androidx.test.runner.AndroidJUnit4;
 
 import static org.junit.Assert.assertEquals;
 
