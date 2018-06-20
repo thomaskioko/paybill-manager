b1c37ee2cadd790f356791b74a644983f612491e
Thomas Kioko <kiokotomas@gmail.com>
2018-06-20 23:08:27 +0300

    bind activity

--- app/src/main/java/com/thomaskioko/paybillmanager/di/module/ActivityModule.kt
+++ app/src/main/java/com/thomaskioko/paybillmanager/di/module/ActivityModule.kt
@@ -1,6 +1,7 @@
 package com.thomaskioko.paybillmanager.di.module
 
 import com.thomaskioko.paybillmanager.ui.IntroActivity
+import com.thomaskioko.paybillmanager.ui.MainActivity
 import com.thomaskioko.paybillmanager.ui.SplashScreenActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -15,5 +16,8 @@ abstract class ActivityModule {
 
     @ContributesAndroidInjector
     abstract fun contributeSplashScreenActivity(): SplashScreenActivity
+
+    @ContributesAndroidInjector(modules = [FragmentBuildersModule::class])
+    abstract fun contributeMainActivity(): MainActivity
 
 }
