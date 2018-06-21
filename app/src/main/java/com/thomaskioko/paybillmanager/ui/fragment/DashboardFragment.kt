174b0de71b06483391b11989317cb4eeec24b8cf
Thomas Kioko <kiokotomas@gmail.com>
2018-06-21 13:23:49 +0300

    create dashboard fragment

---
+++ app/src/main/java/com/thomaskioko/paybillmanager/ui/fragment/DashboardFragment.kt
@@ -0,0 +1,22 @@
+package com.thomaskioko.paybillmanager.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.thomaskioko.paybillmanager.R
+
+
+class DashboardFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+
+}
