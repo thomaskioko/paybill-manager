9fd04ce590447a1bafd711bf0478792f0978a678
Thomas Kioko <kiokotomas@gmail.com>
2018-06-22 00:32:26 +0300

    add debug class and manifest

---
+++ app/src/debug/java/com/thomaskioko/paybillmanager/testing/ui/SingleFragmentActivity.kt
@@ -0,0 +1,37 @@
+package com.thomaskioko.paybillmanager.testing.ui
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.thomaskioko.paybillmanager.R
+
+/**
+ * Used for testing fragments inside a fake activity.
+ */
+class SingleFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val content = FrameLayout(this).apply {
+            layoutParams = FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            id = R.id.container
+        }
+        setContentView(content)
+    }
+
+    fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container, fragment, "TEST")
+            .commit()
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+}
