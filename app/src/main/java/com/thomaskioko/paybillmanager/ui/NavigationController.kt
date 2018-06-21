bce364de4b82c6311d8ca1c9adf063683fa335fd
Thomas Kioko <kiokotomas@gmail.com>
2018-06-21 13:19:41 +0300

    create navigation class

---
+++ app/src/main/java/com/thomaskioko/paybillmanager/ui/NavigationController.kt
@@ -0,0 +1,29 @@
+package com.thomaskioko.paybillmanager.ui
+
+import com.thomaskioko.paybillmanager.R
+import com.thomaskioko.paybillmanager.testing.OpenForTesting
+import com.thomaskioko.paybillmanager.ui.fragment.DashboardFragment
+import javax.inject.Inject
+
+/**
+ * Class responsible for navigation
+ */
+
+
+@OpenForTesting
+class NavigationController
+@Inject constructor(private var mainActivity: MainActivity) {
+
+    private val containerId = R.id.container
+    private val fragmentManager = mainActivity.supportFragmentManager
+
+    fun navigateToDashboardFragment() {
+        val movieListFragment = DashboardFragment()
+        fragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .replace(containerId, movieListFragment)
+                .addToBackStack("DashBoard")
+                .commitAllowingStateLoss()
+    }
+
+}
