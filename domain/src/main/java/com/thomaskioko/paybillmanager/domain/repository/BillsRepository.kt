951e731968813a2c74615bdcd3e2a0faf7cc9f57
Thomas Kioko <kiokotomas@gmail.com>
2018-08-22 02:40:03 +0300

    create repository class

---
+++ domain/src/main/java/com/thomaskioko/paybillmanager/domain/repository/BillsRepository.kt
@@ -0,0 +1,14 @@
+package com.thomaskioko.paybillmanager.domain.repository
+
+import com.thomaskioko.paybillmanager.domain.model.Bill
+import io.reactivex.Completable
+import io.reactivex.Observable
+
+interface BillsRepository {
+
+    fun getBills(): Observable<List<Bill>>
+
+    fun createBill(bill: Bill): Completable
+
+    fun updateBill(billId: String): Completable
+}
