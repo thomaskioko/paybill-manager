5fcea7a836cd55f8177674cd178b3a6cab755939
Thomas Kioko <kiokotomas@gmail.com>
2018-08-22 02:39:00 +0300

    create model class

---
+++ domain/src/main/java/com/thomaskioko/paybillmanager/domain/model/Bill.kt
@@ -0,0 +1,12 @@
+package com.thomaskioko.paybillmanager.domain.model
+
+
+class Bill(
+        var billId: String,
+        var billName: String,
+        var paybillNumber: String,
+        var accountNumber: String,
+        var amount: String,
+        var categoryId: Int,
+        var reminderDate: Long?
+)
