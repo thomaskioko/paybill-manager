52a86f0142de7c901f1a30f83d49d5c7ebd17a35
Thomas Kioko <kiokotomas@gmail.com>
2018-08-22 02:59:39 +0300

    add test cases

---
+++ domain/src/test/java/com/thomaskioko/paybillmanager/domain/interactor/UpdateBillTest.kt
@@ -0,0 +1,50 @@
+package com.thomaskioko.paybillmanager.domain.interactor
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import com.thomaskioko.paybillmanager.domain.bills.UpdateBill
+import com.thomaskioko.paybillmanager.domain.executor.PostExecutionThread
+import com.thomaskioko.paybillmanager.domain.factory.BillsDataFactory
+import com.thomaskioko.paybillmanager.domain.repository.BillsRepository
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+class UpdateBillTest {
+
+    private lateinit var updateBill: UpdateBill
+    @Mock
+    lateinit var billsRepository: BillsRepository
+    @Mock
+    lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        updateBill = UpdateBill(billsRepository, postExecutionThread)
+    }
+
+    @Test
+    fun updateBillCompletes(){
+        stubCreateBillRepository(Completable.complete())
+
+        val testObserver = updateBill.buildUseCaseCompletable(
+                UpdateBill.Params.forBill(BillsDataFactory.randomUuid())
+        ).test()
+
+        //Verify that the observable completes
+        testObserver.assertComplete()
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun updateBillThrowsException(){
+        updateBill.buildUseCaseCompletable().test()
+    }
+
+
+    private fun stubCreateBillRepository(completable: Completable) {
+        whenever(billsRepository.updateBill(any())).thenReturn(completable)
+    }
+}
