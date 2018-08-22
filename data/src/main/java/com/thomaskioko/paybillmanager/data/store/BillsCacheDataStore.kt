f23ae9e70180eea583ce0f3605ccc5c45a21923e
Thomas Kioko <kiokotomas@gmail.com>
2018-08-23 01:11:25 +0300

    create cash dataSource class

---
+++ data/src/main/java/com/thomaskioko/paybillmanager/data/store/BillsCacheDataStore.kt
@@ -0,0 +1,32 @@
+package com.thomaskioko.paybillmanager.data.store
+
+import com.thomaskioko.paybillmanager.data.model.BillEntity
+import com.thomaskioko.paybillmanager.data.repository.BillsCache
+import com.thomaskioko.paybillmanager.data.repository.BillDataStore
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import javax.inject.Inject
+
+open class BillsCacheDataStore @Inject constructor(
+        private val billsCache: BillsCache
+) : BillDataStore {
+
+    override fun createBill(billEntity: BillEntity): Completable {
+        return billsCache.createBill(billEntity)
+    }
+
+    override fun updateBill(billEntity: BillEntity): Completable {
+       return billsCache.updateBill(billEntity)
+    }
+
+    override fun getBills(): Flowable<List<BillEntity>> {
+        return billsCache.getBills()
+    }
+
+
+    override fun deleteBills(): Completable {
+        return billsCache.deleteBills()
+    }
+
+
+}
