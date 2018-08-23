07390e463cf22a9bfbddb4de1593fceed14a2cbc
Thomas Kioko <kiokotomas@gmail.com>
2018-08-23 16:55:17 +0300

    update table name

--- cache/src/main/java/com/thomaskioko/paybillmanager/cache/db/BillsConstants.kt
+++ cache/src/main/java/com/thomaskioko/paybillmanager/cache/db/BillsConstants.kt
@@ -2,21 +2,10 @@ package com.thomaskioko.paybillmanager.cache.db
 
 object BillsConstants {
 
-    const val TABLE_NAME = "projects"
-
-    const val COLUMN_PROJECT_ID = "project_id"
-
-    const val COLUMN_IS_BOOKMARKED = "is_bookmarked"
+    const val TABLE_NAME = "bills"
 
     const val QUERY_BILLS = "SELECT * FROM $TABLE_NAME"
 
     const val DELETE_BILLS = "DELETE FROM $TABLE_NAME"
-
-    const val QUERY_BOOKMARKED_PROJECTS = "SELECT * FROM $TABLE_NAME " +
-            "WHERE $COLUMN_IS_BOOKMARKED = 1"
-
-    const val QUERY_UPDATE_BOOKMARK_STATUS = "UPDATE $TABLE_NAME " +
-            "SET $COLUMN_IS_BOOKMARKED = :isBookmarked WHERE " +
-            "$COLUMN_PROJECT_ID = :projectId"
 
 }
