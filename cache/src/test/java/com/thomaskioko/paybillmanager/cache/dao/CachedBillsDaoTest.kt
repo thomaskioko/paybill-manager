752bb58ace1746495941b8d0b4e75a85d136df12
Thomas Kioko <kiokotomas@gmail.com>
2018-08-23 20:01:36 +0300

    add dao test

---
+++ cache/src/test/java/com/thomaskioko/paybillmanager/cache/dao/CachedBillsDaoTest.kt
@@ -0,0 +1,80 @@
+package com.thomaskioko.paybillmanager.cache.dao
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import com.thomaskioko.paybillmanager.cache.db.PayBillManagerDatabase
+import com.thomaskioko.paybillmanager.cache.factory.BillsCachedFactory
+import com.thomaskioko.paybillmanager.cache.factory.DataFactory
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+
+@RunWith(RobolectricTestRunner::class)
+class CachedBillsDaoTest {
+
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val database = Room.inMemoryDatabaseBuilder(
+            RuntimeEnvironment.application.applicationContext,
+            PayBillManagerDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+    @After
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    fun getBillReturnsData() {
+        val bill = BillsCachedFactory.makeCachedBill()
+        database.billsDao().insertBill(bill)
+
+        val testObserver = database.billsDao().getBills().test()
+        testObserver.assertValue(listOf(bill))
+    }
+
+    @Test
+    fun getBillByIdReturnsData() {
+        val bill = BillsCachedFactory.makeCachedBill()
+        database.billsDao().insertBill(bill)
+
+        val testObserver = database.billsDao().getBill(DataFactory.randomInt()).test()
+        testObserver.assertValue(bill)
+    }
+
+    @Test
+    fun getBillsReturnsData() {
+        val bill = BillsCachedFactory.makeCachedBill()
+        database.billsDao().insertCachedBills(listOf(bill))
+
+        val testObserver = database.billsDao().getBills().test()
+        testObserver.assertValue(listOf(bill))
+    }
+
+    @Test
+    fun getUpdateReturnsData() {
+        val bill = BillsCachedFactory.makeUpdateCachedBill("Zuku")
+        database.billsDao().updateBill(bill)
+
+        val testObserver = database.billsDao().getBills().test()
+        testObserver.assertValue(listOf(bill))
+
+    }
+
+    @Test
+    fun deleteBillsClearsData() {
+        val project = BillsCachedFactory.makeCachedBill()
+        database.billsDao().insertCachedBills(listOf(project))
+        database.billsDao().deleteBills()
+
+        val testObserver = database.billsDao().getBills().test()
+        testObserver.assertValue(emptyList())
+    }
+}
