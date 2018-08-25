a5fa285fd5365d2e6829cca564e9e18d3e42bc55
Thomas Kioko <kiokotomas@gmail.com>
2018-08-25 21:46:34 +0300

    make class open

--- presentation/src/main/java/com/thomaskioko/paybillmanager/presentation/mapper/BillViewMapper.kt
+++ presentation/src/main/java/com/thomaskioko/paybillmanager/presentation/mapper/BillViewMapper.kt
@@ -4,7 +4,7 @@ import com.thomaskioko.paybillmanager.domain.model.Bill
 import com.thomaskioko.paybillmanager.presentation.model.BillView
 import javax.inject.Inject
 
-class BillViewMapper @Inject constructor(): Mapper<BillView, Bill>{
+open class BillViewMapper @Inject constructor(): Mapper<BillView, Bill>{
 
     override fun mapToView(type: Bill): BillView {
        return BillView (type.billId, type.billName, type.paybillNumber, type.accountNumber,
