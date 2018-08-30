c42899b8e2027b32e2bc69cf67b8cfaa20e11f57
Thomas Kioko <kiokotomas@gmail.com>
2018-08-31 00:15:59 +0300

    move toolbar to main activity

--- mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivity.kt
+++ mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivity.kt
@@ -6,12 +6,19 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.thomaskioko.paybillmanager.mobile.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        // Set up ActionBar
+        setSupportActionBar(toolbar_main)
+        NavigationUI.setupActionBarWithNavController(this, navController, null)
 
         val finalHost = NavHostFragment.create(R.navigation.nav_graph)
         supportFragmentManager.beginTransaction()
