c8707d32db2803ba8d43e20d8d1a5ec2d7db9ace
Thomas Kioko <kiokotomas@gmail.com>
2018-08-30 21:47:49 +0300

    implement navigation

--- mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivity.kt
+++ mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.thomaskioko.paybillmanager.mobile.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.thomaskioko.paybillmanager.mobile.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val finalHost = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, finalHost)
+                .setPrimaryNavigationFragment(finalHost) // this is the equivalent to app:defaultNavHost="true"
+                .commit()
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(null,
+                Navigation.findNavController(this, R.id.nav_graph))
+    }
+
 }
