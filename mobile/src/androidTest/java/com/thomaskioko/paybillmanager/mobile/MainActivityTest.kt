439c60d082c0bbfd242a6c3c226a2acbe791832e
Thomas Kioko <kiokotomas@gmail.com>
2018-09-05 17:04:28 +0300

    remove unused imports

--- mobile/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/MainActivityTest.kt
+++ mobile/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/MainActivityTest.kt
@@ -1,13 +1,11 @@
 package com.thomaskioko.paybillmanager.mobile
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.rule.ActivityTestRule
 import com.thomaskioko.paybillmanager.mobile.ui.MainActivity
-import com.thomaskioko.paybillmanager.mobile.util.RecyclerViewMatcher
 import org.hamcrest.CoreMatchers.allOf
 import org.junit.Rule
 import org.junit.Test
