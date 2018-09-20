7e639c387e7ba975bef0f61399a3e23d388cfc42
Thomas Kioko <kiokotomas@gmail.com>
2018-09-20 21:25:06 +0300

    create extensions file

---
+++ mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/extension/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.thomaskioko.paybillmanager.mobile.extension
+
+import android.view.View
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
