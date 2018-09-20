48921ff372393121563092af9af2f9fcb51a638a
Thomas Kioko <kiokotomas@gmail.com>
2018-09-20 21:24:55 +0300

    update ui, add drawer

--- mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivity.kt
+++ mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivity.kt
@@ -1,61 +1,128 @@
 package com.thomaskioko.paybillmanager.mobile.ui
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomappbar.BottomAppBar
+import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.Fragment
+import com.mikepenz.fontawesome_typeface_library.FontAwesome
+import com.mikepenz.google_material_typeface_library.GoogleMaterial
+import com.mikepenz.itemanimators.AlphaCrossFadeAnimator
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.thomaskioko.paybillmanager.mobile.R
-import kotlinx.android.synthetic.main.activity_main.*
+import dagger.android.AndroidInjection
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.DaggerAppCompatActivity
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity(), HasSupportFragmentInjector {
 
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+
+    @Inject
+    lateinit var navigationController: NavigationController
+
+    private var drawer: Drawer? = null
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        AndroidInjection.inject(this)
 
-        // Set up ActionBar
-        setSupportActionBar(toolbar_main)
-        NavigationUI.setupActionBarWithNavController(this, navController, null)
+        navigationController.navigateToBillsListFragment()
 
-        val finalHost = NavHostFragment.create(R.navigation.nav_graph)
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment, finalHost)
-                .setPrimaryNavigationFragment(finalHost) // this is the equivalent to app:defaultNavHost="true"
-                .commit()
+        val profile = ProfileDrawerItem()
+                .withName("")
+                .withEmail("")
+                .withIcon(AppCompatResources.getDrawable(this, R.drawable.ic_launcher_icon))
 
-        val options = NavOptions.Builder()
-                .setEnterAnim(R.anim.slide_in_right)
-                .setExitAnim(R.anim.slide_out_left)
-                .setPopEnterAnim(R.anim.slide_in_left)
-                .setPopExitAnim(R.anim.slide_out_right)
+        // Create the AccountHeader
+        val headerResult = AccountHeaderBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(true)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(profile)
+                .withSelectionListEnabled(false)
                 .build()
 
-        bottom_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+        //Create the drawer
+        drawer = DrawerBuilder()
+                .withActivity(this)
+                .withHasStableIds(true)
+                .withDisplayBelowStatusBar(true)
+                .withItemAnimator(AlphaCrossFadeAnimator())
+                .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+                .addDrawerItems(
+                        PrimaryDrawerItem()
+                                .withName("Home")
+                                .withDescription("View upcoming bills")
+                                .withIcon(FontAwesome.Icon.faw_home)
+                                .withIdentifier(1),
+                        PrimaryDrawerItem()
+                                .withName("Dashboard")
+                                .withDescription("Summary of transactions")
+                                .withIcon(GoogleMaterial.Icon.gmd_account_circle)
+                                .withIdentifier(2),
+                        PrimaryDrawerItem()
+                                .withName("History")
+                                .withDescription("View previous transactions")
+                                .withIcon(FontAwesome.Icon.faw_gamepad)
+                                .withIdentifier(3)
+                )
+                .withOnDrawerItemClickListener { _, _, drawerItem ->
+                    if (drawerItem != null) {
+                    }
+                    false
+                }
+                .build()
+    }
 
-        fab_main.setOnClickListener {
-            if (bottom_bar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER) {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.nav_add_bill_fragment, null, options)
-                bottom_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
-                fab_main.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_back))
-            } else {
-                findNavController(R.id.nav_host_fragment).navigate(R.id.nav_bills_list_fragment, null, options)
-                bottom_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
-                fab_main.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_add))
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            android.R.id.home -> if (drawer != null) {
+                if (drawer!!.isDrawerOpen) {
+                    drawer!!.closeDrawer()
+                } else {
+                    drawer!!.openDrawer()
+                }
+            }
+
+            else -> {
             }
         }
 
+        return super.onOptionsItemSelected(item)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(null,
-                Navigation.findNavController(this, R.id.nav_graph))
+    override fun onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (drawer != null && drawer!!.isDrawerOpen) {
+            drawer!!.closeDrawer()
+        } else {
+            super.onBackPressed()
+        }
+
+        if(supportFragmentManager.backStackEntryCount == 0 ){
+            navigationController.navigateToBillsListFragment()
+        }
     }
+
+    override fun supportFragmentInjector() = dispatchingAndroidInjector
 
 }
