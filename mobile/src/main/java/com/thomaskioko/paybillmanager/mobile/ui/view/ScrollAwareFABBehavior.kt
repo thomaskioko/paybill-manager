76cfc2f66c7abfec0c779e0a0f8a3514b16fb6c3
Thomas Kioko <kiokotomas@gmail.com>
2018-09-27 04:48:37 +0300

    create scroll fab custom class

---
+++ mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/view/ScrollAwareFABBehavior.kt
@@ -0,0 +1,37 @@
+package com.thomaskioko.paybillmanager.mobile.ui.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ScrollAwareFABBehavior(context: Context, attrs: AttributeSet) : FloatingActionButton.Behavior(context, attrs) {
+
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                     child: FloatingActionButton, directTargetChild: View, target: View,
+                                     axes: Int, type: Int): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout,
+                child, directTargetChild, target, axes, type)
+    }
+
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                child: FloatingActionButton, target: View, dxConsumed: Int, dyConsumed: Int,
+                                dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed, type)
+
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide(object : FloatingActionButton.OnVisibilityChangedListener() {
+                override fun onHidden(fab: FloatingActionButton) {
+                    super.onHidden(fab)
+                    fab.visibility = View.INVISIBLE
+                }
+            })
+            // } else if (dyUnconsumed < 0 && child.visibility != View.VISIBLE) {
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
+    }
+}
