a28dd3e98815fef0a4d6f57758c7f637c4210db5
Thomas Kioko <kiokotomas@gmail.com>
2018-09-30 01:05:41 +0300

    update helper functions

--- mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/extension/ViewExtensions.kt
+++ mobile/src/main/java/com/thomaskioko/paybillmanager/mobile/extension/ViewExtensions.kt
@@ -1,6 +1,10 @@
 package com.thomaskioko.paybillmanager.mobile.extension
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.LayoutRes
 
 fun View.show() {
     this.visibility = View.VISIBLE
@@ -9,3 +13,12 @@ fun View.show() {
 fun View.hide() {
     this.visibility = View.GONE
 }
+
+fun TextView.setErrorMessage(errorMessage: String) {
+    this.text = errorMessage
+}
+
+fun ViewGroup.inflate(
+        @LayoutRes layoutRes: Int,
+        attachToRoot: Boolean = false
+): View = LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
