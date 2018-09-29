b182576af91649c96401518196cd67df7bf129c8
Thomas Kioko <kiokotomas@gmail.com>
2018-09-29 23:57:02 +0300

    add activity test

---
+++ mobile/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/ui/MainActivityTest.kt
@@ -0,0 +1,37 @@
+package com.thomaskioko.paybillmanager.mobile.ui
+
+import android.app.Activity
+import android.view.Gravity
+import androidx.appcompat.widget.Toolbar
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.DrawerMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import com.thomaskioko.paybillmanager.mobile.R
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+    @Rule
+    @JvmField
+    var activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun clickDrawerIconOpensAndClosesNavigationDrawer() {
+        Espresso.onView(ViewMatchers.withId(R.id.material_drawer_layout)).check(ViewAssertions.matches(DrawerMatchers.isClosed(Gravity.START)))
+
+        Espresso.onView(ViewMatchers.withContentDescription(getToolbarNavigationContentDescription(
+                activityTestRule.activity, R.id.toolbar_bill_list))).perform(ViewActions.click())
+    }
+
+
+}
+
+
+/**
+ * Returns the content description for the navigation button view in the toolbar.
+ */
+fun getToolbarNavigationContentDescription(activity: Activity, toolbarId: Int) =
+        activity.findViewById<Toolbar>(toolbarId).navigationContentDescription as String
