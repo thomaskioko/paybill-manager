7fe9f01eb6080d1283d451d37027ce7fd829ec51
Thomas Kioko <kiokotomas@gmail.com>
2018-09-30 02:05:06 +0300

    add factory test class

---
+++ mobile/src/test/java/com/thomaskioko/paybillmanager/mobile/factory/BillsFactory.kt
@@ -0,0 +1,17 @@
+package com.thomaskioko.paybillmanager.mobile.factory
+
+import com.thomaskioko.paybillmanager.mobile.factory.DataFactory.randomInt
+import com.thomaskioko.paybillmanager.mobile.factory.DataFactory.randomLong
+import com.thomaskioko.paybillmanager.mobile.factory.DataFactory.randomUuid
+import com.thomaskioko.paybillmanager.presentation.model.BillView
+
+
+object BillsFactory {
+    
+    fun makeBillView(): BillView {
+        return BillView(randomUuid(), randomUuid(), randomUuid(),
+                randomUuid(), randomUuid(), randomInt(),
+                randomLong())
+    }
+
+}
