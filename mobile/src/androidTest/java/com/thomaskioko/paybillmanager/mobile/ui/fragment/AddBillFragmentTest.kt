9074a57d84e6d04e8926dd1a74b2b98cea50a432
Thomas Kioko <kiokotomas@gmail.com>
2018-10-27 19:52:28 +0300

    add ui test

--- mobile/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/ui/fragment/AddBillFragmentTest.kt
+++ mobile/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/ui/fragment/AddBillFragmentTest.kt
@@ -1,43 +1,67 @@
 package com.thomaskioko.paybillmanager.mobile.ui.fragment
 
-import android.app.Activity
-import android.view.Gravity
-import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.MutableLiveData
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.contrib.DrawerMatchers
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
 import com.thomaskioko.paybillmanager.mobile.R
-import com.thomaskioko.paybillmanager.mobile.ui.MainActivity
+import com.thomaskioko.paybillmanager.mobile.SimpleFragmentActivity
+import com.thomaskioko.paybillmanager.mobile.ui.NavigationController
+import com.thomaskioko.paybillmanager.mobile.util.ToolbarViewMarcher
+import com.thomaskioko.paybillmanager.mobile.util.ViewModelUtil
+import com.thomaskioko.paybillmanager.presentation.model.CategoryView
+import com.thomaskioko.paybillmanager.presentation.state.Resource
+import com.thomaskioko.paybillmanager.presentation.viewmodel.category.CreateCategoryViewModel
+import com.thomaskioko.paybillmanager.presentation.viewmodel.category.GetCategoriesViewModel
+import com.thomaskioko.xapotest.util.EspressoTestUtil
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-
+import org.mockito.Mockito
 
 class AddBillFragmentTest {
 
+
     @Rule
     @JvmField
-    var activityTestRule = ActivityTestRule(MainActivity::class.java)
+    val activityRule = ActivityTestRule(SimpleFragmentActivity::class.java, true, true)
+
+    private lateinit var getCategoriesViewModel: GetCategoriesViewModel
+
+    private val liveData = MutableLiveData<Resource<List<CategoryView>>>()
+
+    private lateinit var navigationController: NavigationController
+
+    @Before
+    fun init() {
+        val fragment = AddBillFragment()
+
+        navigationController = Mockito.mock(NavigationController::class.java)
+
+        getCategoriesViewModel = Mockito.mock(GetCategoriesViewModel::class.java)
+        Mockito.`when`(getCategoriesViewModel.getCategories()).thenReturn(liveData)
+
+        fragment.viewModelFactory = ViewModelUtil.createFor(getCategoriesViewModel)
+        activityRule.activity.setFragment(fragment)
+
+        EspressoTestUtil.disableProgressBarAnimations(activityRule)
+
+    }
 
     @Test
-    fun clickDrawerIconOpensAndClosesNavigationDrawer() {
-        Espresso.onView(withId(R.id.material_drawer_layout)).check(ViewAssertions.matches(DrawerMatchers.isClosed(Gravity.START)))
+    fun bill_amount_entered() {
 
-        Espresso.onView(ViewMatchers.withContentDescription(getToolbarNavigationContentDescription(
-                activityTestRule.activity, R.id.toolbar_bill_list))).perform(ViewActions.click())
+        onView(withId(R.id.et_amount))
+                .perform(ViewActions.typeText("2300"), closeSoftKeyboard())
+
     }
 
 
 }
-
-
-/**
- * Returns the content description for the navigation button view in the toolbar.
- */
-fun getToolbarNavigationContentDescription(activity: Activity, toolbarId: Int) =
-        activity.findViewById<Toolbar>(toolbarId).navigationContentDescription as String
