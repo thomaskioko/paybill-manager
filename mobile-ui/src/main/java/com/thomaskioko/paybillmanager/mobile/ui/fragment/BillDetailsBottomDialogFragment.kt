b7a04248732bbb14c000ba682cbd0422937a8a33
Thomas Kioko <kiokotomas@gmail.com>
2018-10-28 00:36:38 +0300

    fix keyboard overlapping issue

--- mobile-ui/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/fragment/BillDetailsBottomDialogFragment.kt
+++ mobile-ui/src/main/java/com/thomaskioko/paybillmanager/mobile/ui/fragment/BillDetailsBottomDialogFragment.kt
@@ -1,19 +1,13 @@
 package com.thomaskioko.paybillmanager.mobile.ui.fragment
 
 
-import android.annotation.SuppressLint
-import android.app.Dialog
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.FrameLayout
+import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.thomaskioko.paybillmanager.mobile.R
 import com.thomaskioko.paybillmanager.mobile.injection.Injectable
@@ -25,6 +19,11 @@ import org.threeten.bp.OffsetDateTime
 
 class BillDetailsBottomDialogFragment : BottomSheetDialogFragment(), Injectable, DaysAdapter.OnRecyclerViewItemClickListener {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -46,26 +45,7 @@ class BillDetailsBottomDialogFragment : BottomSheetDialogFragment(), Injectable,
 
     }
 
-    @SuppressLint("RestrictedApi")
-    override fun setupDialog(dialog: Dialog?, style: Int) {
-        super.setupDialog(dialog, style)
-
-        activity!!.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-        dialog!!.setOnShowListener {
-            val handler = Handler()
-            handler.postDelayed({
-                val bottomSheetDialog = dialog as BottomSheetDialog
-                val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
-                BottomSheetBehavior.from(bottomSheet!!).state = BottomSheetBehavior.STATE_EXPANDED
-            }, 250)
-        }
-    }
 
     override fun selectedDateItem(offsetDateTime: OffsetDateTime) {
-    }
-
-    companion object {
-        const val BUNDLE_AMOUNT = "bundle_amount"
-        const val BUNDLE_CATEGORY_ID = "bundle_category_id"
     }
 }
