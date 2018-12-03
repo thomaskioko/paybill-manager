a99472fa9170d63a89bcdad27d77d002bbd08e71
Thomas Kioko <kiokotomas@gmail.com>
2018-12-03 18:39:33 +0300

    Rename class

--- mobile-ui/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/util/matcher/TextInputLayoutMatcher.kt
+++ mobile-ui/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/util/matcher/TextInputLayoutMatcher.kt
@@ -1,4 +1,4 @@
-package com.thomaskioko.paybillmanager.mobile.util
+package com.thomaskioko.paybillmanager.mobile.util.matcher
 
 import android.view.View
 import androidx.test.espresso.core.internal.deps.guava.base.Preconditions
@@ -8,7 +8,7 @@ import org.hamcrest.CoreMatchers
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 
-object EspressoTextInputLayoutUtils {
+object TextInputLayoutMatcher {
 
     fun withErrorInInputLayout(stringMatcher: Matcher<String>): Matcher<View> {
         Preconditions.checkNotNull(stringMatcher)
@@ -36,5 +36,29 @@ object EspressoTextInputLayoutUtils {
 
     fun withErrorInInputLayout(string: String): Matcher<View> {
         return withErrorInInputLayout(CoreMatchers.`is`(string))
+    }
+
+    fun hasTextInputLayoutHintText(stringMatcher: Matcher<String>): Matcher<View> {
+        Preconditions.checkNotNull(stringMatcher)
+
+        return object : BoundedMatcher<View, TextInputLayout>(TextInputLayout::class.java) {
+            var actualHint = ""
+
+            override fun describeTo(description: Description) {
+                description.appendText("with hint: ")
+                stringMatcher.describeTo(description)
+
+                description.appendText("But got: $stringMatcher.describeTo(description)")
+            }
+
+            public override fun matchesSafely(textInputLayout: TextInputLayout): Boolean {
+                val hint = textInputLayout.hint
+                if (hint != null) {
+                    actualHint = hint.toString()
+                    return stringMatcher.matches(actualHint)
+                }
+                return false
+            }
+        }
     }
 }
