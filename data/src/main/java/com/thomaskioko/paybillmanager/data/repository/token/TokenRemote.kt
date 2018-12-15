c8fe469cd4501b37f5dbafd9fb434462d8fd8aca
Thomas Kioko <kiokotomas@gmail.com>
2018-12-15 16:41:39 +0300

    remove safaricom reference - replace with jenga

--- data/src/main/java/com/thomaskioko/paybillmanager/data/repository/token/TokenRemote.kt
+++
@@ -1,9 +0,0 @@
-package com.thomaskioko.paybillmanager.data.repository.token
-
-import com.thomaskioko.paybillmanager.data.model.SafaricomTokenEntity
-import io.reactivex.Flowable
-
-interface TokenRemote {
-
-    fun getSafaricomToken(): Flowable<SafaricomTokenEntity>
-}
--- domain/src/main/java/com/thomaskioko/paybillmanager/domain/model/SafaricomToken.kt
+++
@@ -1,7 +0,0 @@
-package com.thomaskioko.paybillmanager.domain.model
-
-data class SafaricomToken(
-        var id: Int,
-        var expiresIn: Long,
-        var accessToken: String
-)
