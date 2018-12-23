6738ce788dca93951ca0ddc12b8206f49dc92b3f
Thomas Kioko <kiokotomas@gmail.com>
2018-12-23 21:26:20 +0300

    add date format functions

--- mobile-ui/src/main/java/com/thomaskioko/paybillmanager/mobile/util/DateUtils.kt
+++ mobile-ui/src/main/java/com/thomaskioko/paybillmanager/mobile/util/DateUtils.kt
@@ -3,6 +3,8 @@ package com.thomaskioko.paybillmanager.mobile.util
 import org.threeten.bp.Instant
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.ZoneId
+import java.text.SimpleDateFormat
+import java.util.*
 
 object DateUtils {
 
@@ -22,5 +24,34 @@ object DateUtils {
 
         return days
 
+    }
+
+
+    fun formatTimeStampToDate(timeStamp: Long): String {
+
+        val instance = Instant.EPOCH.plusMillis(timeStamp)
+        val offsetTime = OffsetDateTime.ofInstant(instance, ZoneId.of("GMT+03:00"))
+
+        val day = offsetTime.dayOfMonth
+        val month = StringUtils.upperCaseFirstLetter(offsetTime.month.name.toLowerCase())
+
+        if (day !in 11..18)
+            return when (day % 10) {
+                1 -> "${day}st $month"
+                2 -> "${day}nd $month"
+                3 -> "${day}rd $month"
+                else -> "${day}th $month"
+            }
+        return "${day}th $month"
+
+    }
+
+    fun dateToTimeStamp(formattedDate: String): Long {
+
+        val dateFormat = SimpleDateFormat("MM-dd-yyyy HH:mm:ss", Locale.getDefault())
+        dateFormat.timeZone = TimeZone.getTimeZone("GMT+03:00")
+
+        val parsedDate = dateFormat.parse(formattedDate)
+        return parsedDate.time
     }
 }
