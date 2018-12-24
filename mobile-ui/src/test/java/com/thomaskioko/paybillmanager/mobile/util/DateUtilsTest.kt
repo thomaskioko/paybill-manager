3175898a04f4fcea8d607aeb0b4fe4b06d84996a
Thomas Kioko <kiokotomas@gmail.com>
2018-12-24 21:39:01 +0300

    add tests

--- mobile-ui/src/test/java/com/thomaskioko/paybillmanager/mobile/util/DateUtilsTest.kt
+++ mobile-ui/src/test/java/com/thomaskioko/paybillmanager/mobile/util/DateUtilsTest.kt
@@ -36,10 +36,28 @@ class DateUtilsTest {
     @Test
     fun timStampFormatsDateToString(){
         //Given a timestamp
-        val formattedDate = formatTimeStampToDate(1545583939000)
+        val formattedDateSt = formatTimeStampToDate(1538412720000)
+
+        //Verify that the formatted date is the correct format with `th`
+        assertEquals("1st October", formattedDateSt)
+
+        //Given a timestamp
+        val formattedDateTh = formatTimeStampToDate(1545674983)
+
+        //Verify that the formatted date is the correct format with `th`
+        assertEquals("19th January", formattedDateTh)
+
+        //Given a timestamp
+        val formattedDateNd = formatTimeStampToDate(1545497520000)
+
+        //Verify that the formatted date is the correct format with `nd`
+        assertEquals("22nd December", formattedDateNd)
+
+        //Given a timestamp
+        val formattedDateDec = formatTimeStampToDate(1545583939000)
 
         //Verify that the formatted date is the correct format
-        assertEquals("23rd December", formattedDate)
+        assertEquals("23rd December", formattedDateDec)
 
     }
 
@@ -49,6 +67,35 @@ class DateUtilsTest {
         val formattedDate = getMonthFromTimeStamp(1545583939000)
 
         //Verify that the formatted date is the correct format
+        assertEquals("23rd", formattedDate)
+
+    }
+
+    @Test
+    fun getMonthFromTimeStampReturnsTheRightFormat(){
+
+        //Given a timestamp
+        val formattedDateSt = getMonthFromTimeStamp(1543683120000)
+
+        //Verify that the formatted date is the correct format with `th`
+        assertEquals("1st", formattedDateSt)
+
+        //Given a timestamp
+        val formattedDateTh = getMonthFromTimeStamp(1545674983)
+
+        //Verify that the formatted date is the correct format with `th`
+        assertEquals("19th", formattedDateTh)
+
+        //Given a timestamp
+        val formattedDateNd = getMonthFromTimeStamp(1545497520000)
+
+        //Verify that the formatted date is the correct format with `nd`
+        assertEquals("22nd", formattedDateNd)
+
+        //Given a timestamp
+        val formattedDate = getMonthFromTimeStamp(1545583939000)
+
+        //Verify that the formatted date is the correct format with `rd`
         assertEquals("23rd", formattedDate)
 
     }
