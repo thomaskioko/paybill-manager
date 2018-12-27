865c5ffaa5bbaf0de32bdc6bd585201aa1d5ce48
Thomas Kioko <kiokotomas@gmail.com>
2018-12-27 21:05:54 +0300

    delete token view

--- presentation/src/main/java/com/thomaskioko/paybillmanager/presentation/model/TokenView.kt
+++
@@ -1,7 +0,0 @@
-package com.thomaskioko.paybillmanager.presentation.model
-
-class TokenView(
-        var id: Int,
-        var expiresIn: Long,
-        var accessToken: String
-)
--- presentation/src/test/java/com/thomaskioko/paybillmanager/presentation/factory/TokenFactory.kt
+++ presentation/src/test/java/com/thomaskioko/paybillmanager/presentation/factory/TokenFactory.kt
@@ -2,14 +2,10 @@ package com.thomaskioko.paybillmanager.presentation.factory
 
 import com.thomaskioko.paybillmanager.domain.model.JengaToken
 import com.thomaskioko.paybillmanager.presentation.model.JengaTokenView
-import com.thomaskioko.paybillmanager.presentation.model.TokenView
 
 
 object TokenFactory {
 
-    fun makeSafaricomTokenView(): TokenView {
-        return TokenView(DataFactory.randomInt(), DataFactory.randomLong(), DataFactory.randomUuid())
-    }
 
     fun makeJengaToken(): JengaToken {
         return JengaToken("bearer", "1544517293525", "3599", "EJ4CSPoMBIYAj8KLUp45d5CUflvm9lz")
