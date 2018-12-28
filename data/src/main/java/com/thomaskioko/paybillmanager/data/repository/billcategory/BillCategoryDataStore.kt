9c27847c7f268148fd60c534f54c526be163a7c0
Thomas Kioko <kiokotomas@gmail.com>
2018-12-28 21:20:18 +0300

    add interface functions

--- data/src/main/java/com/thomaskioko/paybillmanager/data/repository/billcategory/BillCategoryDataStore.kt
+++ data/src/main/java/com/thomaskioko/paybillmanager/data/repository/billcategory/BillCategoryDataStore.kt
@@ -1,6 +1,8 @@
 package com.thomaskioko.paybillmanager.data.repository.billcategory
 
 import com.thomaskioko.paybillmanager.data.model.BillCategoryEntity
+import com.thomaskioko.paybillmanager.data.model.BillEntity
+import com.thomaskioko.paybillmanager.data.model.CategoryEntity
 import io.reactivex.Completable
 import io.reactivex.Flowable
 
@@ -9,6 +11,10 @@ interface BillCategoryDataStore {
     fun createBillCategory(billCategoryEntity: BillCategoryEntity): Completable
 
     fun getBillCategory(billId: String, categoryId: String): Flowable<BillCategoryEntity>
+
+    fun getBillsByCategoryId(categoryId: String): Flowable<List<BillEntity>>
+
+    fun getCategoryByBillId(billId: String): Flowable<CategoryEntity>
 
     fun updateBillCategory(billCategoryEntity: BillCategoryEntity): Completable
 }
--- domain/src/main/java/com/thomaskioko/paybillmanager/domain/repository/BillCategoryRepository.kt
+++ domain/src/main/java/com/thomaskioko/paybillmanager/domain/repository/BillCategoryRepository.kt
@@ -1,9 +1,19 @@
 package com.thomaskioko.paybillmanager.domain.repository
 
+import com.thomaskioko.paybillmanager.domain.model.Bill
 import com.thomaskioko.paybillmanager.domain.model.BillCategory
+import com.thomaskioko.paybillmanager.domain.model.Category
+import io.reactivex.Completable
 import io.reactivex.Observable
 
 interface BillCategoryRepository {
 
     fun getBillCategory(billId: String, categoryId: String): Observable<BillCategory>
+
+    fun getBillsByCategoryId(categoryId: String): Observable<List<Bill>>
+
+    fun getCategoryByBillId(billId: String): Observable<Category>
+
+    fun createBillCategory(billCategory: BillCategory): Completable
+
 }
