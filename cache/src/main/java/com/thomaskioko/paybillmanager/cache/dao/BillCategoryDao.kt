839087f71a99d5a48feb908ffcd5d5aade2bd3a8
Thomas Kioko <kiokotomas@gmail.com>
2018-12-28 21:20:43 +0300

    add dao functions

--- cache/src/main/java/com/thomaskioko/paybillmanager/cache/dao/BillCategoryDao.kt
+++ cache/src/main/java/com/thomaskioko/paybillmanager/cache/dao/BillCategoryDao.kt
@@ -16,12 +16,19 @@ abstract class BillCategoryDao {
     @JvmSuppressWildcards
     abstract fun insertBillCategory(cachedBillCategory: CachedBillCategory)
 
+    @Query("SELECT * FROM bill_category_join WHERE billId=:billId AND categoryId=:categoryId")
+    abstract fun getBillCategory(billId: String, categoryId: String): Flowable<CachedBillCategory>
+
     @Query("SELECT * FROM bill INNER JOIN bill_category_join ON " +
             "bill.id=bill_category_join.billId WHERE bill_category_join.categoryId=:categoryId")
-    abstract fun getBill(categoryId: String): Flowable<List<CachedBill>>
+    abstract fun getBills(categoryId: String): Flowable<List<CachedBill>>
 
     @Query("SELECT * FROM category INNER JOIN bill_category_join ON " +
             "category.id=bill_category_join.categoryId WHERE bill_category_join.billId=:billId")
     abstract fun getCategory(billId: String): Flowable<CachedCategory>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @JvmSuppressWildcards
+    abstract fun updateBillCategory(cachedBillCategory: CachedBillCategory)
 
 }
