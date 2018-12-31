5be3f8900cd7a84775a117678d0a905b8af6f5a6
Thomas Kioko <kiokotomas@gmail.com>
2018-12-31 21:05:22 +0300

    inject cache data store

--- data/src/main/java/com/thomaskioko/paybillmanager/data/store/billcategory/BillCategoryDataStoreFactory.kt
+++ data/src/main/java/com/thomaskioko/paybillmanager/data/store/billcategory/BillCategoryDataStoreFactory.kt
@@ -3,9 +3,11 @@ package com.thomaskioko.paybillmanager.data.store.billcategory
 import com.thomaskioko.paybillmanager.data.repository.billcategory.BillCategoryDataStore
 import javax.inject.Inject
 
-open class BillCategoryDataStoreFactory @Inject
-constructor(
-        private val billCategoryDataStore: BillCategoryDataStore
+/**
+ * Helper class that decides which data source to use.
+ */
+open class BillCategoryDataStoreFactory @Inject constructor(
+        private val billCategoryDataStore: BillCategoryCacheDataStore
 ) {
     open fun getCacheDataStore(): BillCategoryDataStore {
         return billCategoryDataStore
