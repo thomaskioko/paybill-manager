252cf7de8d63086d0cb04669e002c9da0e38e005
Thomas Kioko <kiokotomas@gmail.com>
2019-01-03 00:16:57 +0300

    update required param

--- data/src/test/java/com/thomaskioko/paybillmanager/data/test/store/billcategory/BillCategoryDataStoreFactoryTest.kt
+++ data/src/test/java/com/thomaskioko/paybillmanager/data/test/store/billcategory/BillCategoryDataStoreFactoryTest.kt
@@ -2,13 +2,14 @@ package com.thomaskioko.paybillmanager.data.test.store.billcategory
 
 import com.nhaarman.mockitokotlin2.mock
 import com.thomaskioko.paybillmanager.data.repository.billcategory.BillCategoryDataStore
+import com.thomaskioko.paybillmanager.data.store.billcategory.BillCategoryCacheDataStore
 import com.thomaskioko.paybillmanager.data.store.billcategory.BillCategoryDataStoreFactory
 import junit.framework.TestCase
 import org.junit.Test
 
 class BillCategoryDataStoreFactoryTest {
 
-    private val cacheStore = mock<BillCategoryDataStore>()
+    private val cacheStore = mock<BillCategoryCacheDataStore>()
     private val factory = BillCategoryDataStoreFactory(cacheStore)
 
 
