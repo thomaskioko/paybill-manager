1958054967d43a8d527fbf776d0583ad13975fb8
Thomas Kioko <kiokotomas@gmail.com>
2019-01-12 12:55:10 +0300

    add interface functions

--- domain/src/main/java/com/thomaskioko/paybillmanager/domain/repository/MpesaRequestRepository.kt
+++ domain/src/main/java/com/thomaskioko/paybillmanager/domain/repository/MpesaRequestRepository.kt
@@ -2,8 +2,17 @@ package com.thomaskioko.paybillmanager.domain.repository
 
 import com.thomaskioko.paybillmanager.domain.model.MpesaPushResponse
 import com.thomaskioko.paybillmanager.domain.model.mpesa.MpesaPushRequest
+import io.reactivex.Completable
 import io.reactivex.Flowable
 
+/**
+ * Interface defining methods for how the data layer can pass data to and from the Domain layer.
+ * This is to be implemented by the data layer, setting the requirements for the
+ * operations that need to be implemented
+ */
 interface MpesaRequestRepository {
-    fun createMpesaPushRequest(mpesaPushRequest: MpesaPushRequest): Flowable<MpesaPushResponse>
+
+    fun getMpesaStkPush(mpesaPushRequest: MpesaPushRequest): Flowable<MpesaPushResponse>
+
+    fun saveMpesaPushResponse(mpesaPushResponse: MpesaPushResponse): Completable
 }
