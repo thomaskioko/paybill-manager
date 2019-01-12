becce3a3f288bbe502282ea8cc5b0e66daa18e18
Thomas Kioko <kiokotomas@gmail.com>
2019-01-12 12:54:32 +0300

    add mapper test class

---
+++ data/src/test/java/com/thomaskioko/paybillmanager/data/test/mapper/MpesaPushResponseMapperTest.kt
@@ -0,0 +1,37 @@
+package com.thomaskioko.paybillmanager.data.test.mapper
+
+import com.thomaskioko.paybillmanager.data.mapper.MpesaPushResponseMapper
+import com.thomaskioko.paybillmanager.data.model.MpesaPushResponseEntity
+import com.thomaskioko.paybillmanager.data.test.factory.DataFactory
+import com.thomaskioko.paybillmanager.domain.model.MpesaPushResponse
+import junit.framework.TestCase
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MpesaPushResponseMapperTest {
+
+    private val mapper = MpesaPushResponseMapper()
+
+    @Test
+    fun mapFromEntityMapsData() {
+        val entity = DataFactory.makeMpesaPushResponseEntity()
+        val model = mapper.mapFromEntity(entity)
+
+        assertEqualData(entity, model)
+    }
+
+    @Test
+    fun mapToEntityMapsData() {
+        val model = DataFactory.makeMpesaPushResponse()
+        val entity = mapper.mapToEntity(model)
+
+        assertEqualData(entity, model)
+    }
+
+    private fun assertEqualData(entity: MpesaPushResponseEntity, model: MpesaPushResponse) {
+        TestCase.assertEquals(entity.transactionRef, model.transactionRef)
+        TestCase.assertEquals(entity.status, model.status)
+    }
+}
