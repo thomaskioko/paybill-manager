6e566f19908754222da665dcb8478e474afff07f
Thomas Kioko <kiokotomas@gmail.com>
2019-01-12 12:54:50 +0300

    add store unit test class

---
+++ data/src/test/java/com/thomaskioko/paybillmanager/data/test/store/mpesapush/MpesaPushDataStoreFactoryTest.kt
@@ -0,0 +1,67 @@
+package com.thomaskioko.paybillmanager.data.test.store.mpesapush
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.thomaskioko.paybillmanager.data.repository.mpesapush.MpesaPushCache
+import com.thomaskioko.paybillmanager.data.store.mpesapush.MpesaPushCacheDataStore
+import com.thomaskioko.paybillmanager.data.store.mpesapush.MpesaPushDataStoreFactory
+import com.thomaskioko.paybillmanager.data.store.mpesapush.MpesaPushRemoteDataStore
+import io.reactivex.Single
+import junit.framework.TestCase.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MpesaPushDataStoreFactoryTest {
+
+    private lateinit var dataStoreFactory: MpesaPushDataStoreFactory
+
+    private lateinit var cache: MpesaPushCache
+    private lateinit var cacheDataStore: MpesaPushCacheDataStore
+    private lateinit var remoteDataStore: MpesaPushRemoteDataStore
+
+    @Before
+    fun setUp() {
+        cache = mock()
+        cacheDataStore = mock()
+        remoteDataStore = mock()
+        dataStoreFactory = MpesaPushDataStoreFactory(cacheDataStore, remoteDataStore)
+    }
+
+    @Test
+    fun retrieveDataStoreWhenNotCachedReturnsRemoteDataStore() {
+        stubIsStkResponseCached(Single.just(false))
+        val dataStore = dataStoreFactory.getDataStore(false)
+
+        assertEquals(dataStore, remoteDataStore)
+    }
+
+    @Test
+    fun retrieveDataStoreReturnsCacheDataStore() {
+        stubIsStkResponseCached(Single.just(true))
+        val dataStore = dataStoreFactory.getDataStore(true)
+
+        assertEquals(dataStore, cacheDataStore)
+    }
+
+    @Test
+    fun retrieveCacheDataStoreReturnsCacheDataStore() {
+        val dataStore = dataStoreFactory.retrieveCacheDataStore()
+        assert(dataStore is MpesaPushCacheDataStore)
+    }
+
+    @Test
+    fun retrieveRemoteDataStoreReturnsCacheDataStore() {
+        val dataStore = dataStoreFactory.retrieveRemoteDataStore()
+        assert(dataStore is MpesaPushRemoteDataStore)
+    }
+
+
+    private fun stubIsStkResponseCached(isCached: Single<Boolean>) {
+        whenever(cache.isStkResponseCached())
+                .thenReturn(isCached)
+
+    }
+}
---
+++ data/src/test/java/com/thomaskioko/paybillmanager/data/test/store/mpesapush/MpesaPushRemoteDataStoreTest.kt
@@ -0,0 +1,40 @@
+package com.thomaskioko.paybillmanager.data.test.store.mpesapush
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.thomaskioko.paybillmanager.data.model.MpesaPushResponseEntity
+import com.thomaskioko.paybillmanager.data.repository.mpesapush.MpesaPushRemote
+import com.thomaskioko.paybillmanager.data.store.mpesapush.MpesaPushRemoteDataStore
+import com.thomaskioko.paybillmanager.data.test.factory.DataFactory
+import io.reactivex.Flowable
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class MpesaPushRemoteDataStoreTest {
+
+    private val remote = mock<MpesaPushRemote>()
+    private val store = MpesaPushRemoteDataStore(remote)
+
+    @Test
+    fun createMpesaPushRequestCompletes(){
+
+        stubGetMpesaStkPushRequest(Flowable.just(DataFactory.makeMpesaPushResponseEntity()))
+
+        val testObserver = store.getMpesaStkPushRequest(DataFactory.makeMpesaPushRequestEntity()).test()
+        testObserver.assertComplete()
+
+    }
+
+    @Test(expected = UnsupportedOperationException::class)
+    fun saveMpesaPushResponseThrowsException(){
+       store.saveMpesaPushResponse(DataFactory.makeMpesaPushResponseEntity()).test()
+    }
+
+    private fun stubGetMpesaStkPushRequest(observer: Flowable<MpesaPushResponseEntity>){
+        whenever(remote.getMpesaStkPushRequest(any()))
+                .thenReturn(observer)
+    }
+}
