e4d7aa3ad76613b2c086002849fa6db4b29139eb
Thomas Kioko <kiokotomas@gmail.com>
2019-01-15 02:51:16 +0300

    add mock classes

--- remote/src/test/java/com/thomaskioko/paybillmanager/remote/factory/TestDataFactory.kt
+++ remote/src/test/java/com/thomaskioko/paybillmanager/remote/factory/TestDataFactory.kt
@@ -1,7 +1,9 @@
 package com.thomaskioko.paybillmanager.remote.factory
 
 import com.thomaskioko.paybillmanager.data.model.JengaTokenEntity
+import com.thomaskioko.paybillmanager.data.model.MpesaPushResponseEntity
 import com.thomaskioko.paybillmanager.remote.model.JengaToken
+import com.thomaskioko.paybillmanager.remote.model.MpesaPushResponse
 import java.util.*
 
 object TestDataFactory {
@@ -23,6 +25,20 @@ object TestDataFactory {
 
     fun makeJengaTokenEntity(): JengaTokenEntity {
         return JengaTokenEntity("bearer", "1544517293525", "3599", "EJ4CSPoMBIYAj8KLUp45d5CUflvm9lz")
+    }
+
+    fun makeMpesaPushResponseEntity(): MpesaPushResponseEntity {
+        return MpesaPushResponseEntity(
+                "ws_CO_DMZ_215811302_09012019022651831",
+                "Success. Request accepted for processing"
+        )
+    }
+
+    fun makeMpesaPushResponse(): MpesaPushResponse {
+        return MpesaPushResponse(
+                "ws_CO_DMZ_215811302_09012019022651831",
+                "Success. Request accepted for processing"
+        )
     }
 
 }
