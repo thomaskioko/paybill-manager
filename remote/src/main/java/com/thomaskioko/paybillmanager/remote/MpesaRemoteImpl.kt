d30e30c7a7b493fc5733e5240afc631ae4a0502b
Thomas Kioko <kiokotomas@gmail.com>
2019-01-15 03:44:25 +0300

    create remote implementation class

---
+++ remote/src/main/java/com/thomaskioko/paybillmanager/remote/MpesaRemoteImpl.kt
@@ -0,0 +1,22 @@
+package com.thomaskioko.paybillmanager.remote
+
+import com.thomaskioko.paybillmanager.data.model.MpesaPushResponseEntity
+import com.thomaskioko.paybillmanager.data.repository.mpesapush.MpesaPushRemote
+import com.thomaskioko.paybillmanager.domain.model.mpesa.MpesaPushRequest
+import com.thomaskioko.paybillmanager.remote.mapper.MpesaPushResponseMapper
+import com.thomaskioko.paybillmanager.remote.service.JengaService
+import io.reactivex.Flowable
+import javax.inject.Inject
+
+open class MpesaRemoteImpl @Inject constructor(
+        private val responseMapper: MpesaPushResponseMapper,
+        private val service: JengaService
+) : MpesaPushRemote {
+
+    override fun getMpesaStkPushRequest(bearerToken: String, mpesaPushRequestEntity: MpesaPushRequest): Flowable<MpesaPushResponseEntity> {
+        return service.getMpesaStkPush(bearerToken, mpesaPushRequestEntity.toString())
+                .map {
+                    responseMapper.mapFromModel(it)
+                }
+    }
+}
