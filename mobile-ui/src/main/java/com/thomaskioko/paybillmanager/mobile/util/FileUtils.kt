f718e998c1679828c2ef242ae07eb3d4b51dc089
Thomas Kioko <kiokotomas@gmail.com>
2019-01-18 17:03:46 +0300

    create file util class

---
+++ mobile-ui/src/main/java/com/thomaskioko/paybillmanager/mobile/util/FileUtils.kt
@@ -0,0 +1,19 @@
+package com.thomaskioko.paybillmanager.mobile.util
+
+import android.content.Context
+import timber.log.Timber
+import java.io.InputStream
+import javax.inject.Inject
+
+class FileUtils @Inject constructor(val context: Context) {
+
+    fun loadFile(): String? {
+        return try {
+            val inputStream: InputStream = context.assets.open("pem/pkcs8_privatekey.pem")
+            inputStream.bufferedReader().use { it.readText() }.toString()
+        } catch (exception: Exception) {
+            Timber.e("@loadFile $exception ")
+            null
+        }
+    }
+} 
