e3deaa76e9d023075d88242b712f21aa03f6bc56
Thomas Kioko <kiokotomas@gmail.com>
2019-01-19 14:56:46 +0300

    cleanup test

--- mobile-ui/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/ui/fragment/PaymentFragmentTest.kt
+++ mobile-ui/src/androidTest/java/com/thomaskioko/paybillmanager/mobile/ui/fragment/PaymentFragmentTest.kt
@@ -7,12 +7,13 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import com.thomaskioko.paybillmanager.mobile.R
 import com.thomaskioko.paybillmanager.mobile.SimpleFragmentActivity
 import com.thomaskioko.paybillmanager.mobile.util.EspressoAnimationTestUtil
 import com.thomaskioko.paybillmanager.mobile.util.ViewModelUtil.createViewModelFactory
+import com.thomaskioko.paybillmanager.presentation.model.CategoryView
 import com.thomaskioko.paybillmanager.presentation.model.JengaTokenView
+import com.thomaskioko.paybillmanager.presentation.model.MpesaPushResponseView
 import com.thomaskioko.paybillmanager.presentation.state.Resource
 import com.thomaskioko.paybillmanager.presentation.viewmodel.JengaRequestsViewModel
 import com.thomaskioko.paybillmanager.presentation.viewmodel.bill.GetBillsViewModel
@@ -21,8 +22,6 @@ import org.hamcrest.core.IsNot
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
@@ -38,6 +37,8 @@ class PaymentFragmentTest {
     private lateinit var getBillsViewModel: GetBillsViewModel
 
     private val liveData = MutableLiveData<Resource<JengaTokenView>>()
+    private val mpesaStkLiveData = MutableLiveData<Resource<MpesaPushResponseView>>()
+    private val categoryLiveData = MutableLiveData<Resource<CategoryView>>()
 
 
     @Before
@@ -50,13 +51,17 @@ class PaymentFragmentTest {
 
         `when`(jengaRequestViewModel.getJengaToken()).thenReturn(liveData)
         `when`(jengaRequestViewModel.liveData).thenReturn(liveData)
+        `when`(jengaRequestViewModel.getMpesaPushResponse()).thenReturn(mpesaStkLiveData)
+        `when`(jengaRequestViewModel.mpesaStkLiveData).thenReturn(mpesaStkLiveData)
+
+        `when`(getBillCategoryViewModel.getCategoryByBillId()).thenReturn(categoryLiveData)
+        `when`(getBillCategoryViewModel.categoryLiveData).thenReturn(categoryLiveData)
 
         fragment.viewModelFactory = createViewModelFactory(jengaRequestViewModel)
         fragment.viewModelFactory = createViewModelFactory(getBillCategoryViewModel)
         fragment.viewModelFactory = createViewModelFactory(getBillsViewModel)
 
         activityRule.activity.setFragment(fragment)
-
         activityRule.runOnUiThread { }
 
         EspressoAnimationTestUtil.disableProgressBarAnimations(activityRule)
